b675a7683c3d2100ed1b4fe4d4b0710603a0a175
Jay Singh <72841224+jays1ngh@users.noreply.github.com>
2021-04-16 14:54:50 +1000

    Added function Write-Log

--- Logging/Write-Log.ps1
+++ Logging/Write-Log.ps1
@@ -1,1 +1,37 @@
 
+function Write-Log {
+    [CmdletBinding()]
+     param(
+     [Parameter()]
+     [ValidateNotNullOrEmpty()]
+     [string]$Message,
+     [parameter(Mandatory=$true)]
+     [String]$Component,
+     [Parameter(Mandatory=$true)]
+     [ValidateSet("Info", "Warning", "Error")]
+     [String]$Type
+     )
+
+     switch ($Type) {
+        "Info" { [int]$Type = 1 }
+        "Warning" { [int]$Type = 2 }
+        "Error" { [int]$Type = 3 }
+        }
+
+$ProgramDataPath = ($env:ProgramData)
+$LogFile = "$ProgramDataPath\CCMCache\Logs\CCMCache.log"
+
+if(-not(Test-Path $LogFile))
+    {
+        New-Item -Name CCMCache.log -ItemType File `
+        -Path "$ProgramDataPath\CCMCache\Logs\" -Force
+    }
+    
+    $LogTime = "$(Get-Date -Format HH:mm:ss).$((Get-Date).Millisecond)+000"
+    $LogDate = (Get-Date -Format MM-dd-yyyy)
+    $LogEntry = '<![LOG[{0}]LOG]!><time="{1}" date="{2}" component="{3}" context="" type="{4}" thread="" file="">'
+    $LogFormat = $Message, $LogTime, $LogDate, $Component, $Type
+    $LogEntry = $LogEntry -f $LogFormat
+    Add-Content -Value $LogEntry -Path $LogFile
+
+} #write-Log
